62b0bf41e9e44eb481b0144ae379f86afc34312d
YuriBortolato <yurigbort@gmail.com>
2026-05-05 22:25:05 -0300

    feat: add JWT authentication and login route

--- APIV2.py
+++ APIV2.py
@@ -1,10 +1,17 @@
 from importlib.metadata import requires
-
+from datetime import timedelta
 from flask import Flask, jsonify, request
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from marshmallow import Schema, fields, ValidationError
 
 # Criar aplicação flask
 app = Flask(__name__)
+
+# Fazer configuraçao do token JWT
+# Em produçao NUNCA deixe essa chave hard coded!!!
+app.config['JWT_SECRET_KEY'] = 'super-secret'
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(minutes=2)
+jwt = JWTManager
 
 # Criação do banco de dados fictício
 usuarios = {
@@ -12,6 +19,23 @@ usuarios = {
     "2":{"nome":"Barbie Pedreira","email":"bs2ken@gmail.com","senha":"4321"},
     "3":{"nome":"Miranha","email":"peterparker@uol.com.br","senha":"mudar123"}
 }
+
+# Rota de login
+@app.route('/login', methods=['POST'])
+def login():
+    # Obtem as crendiciarias pelo usuario
+    email = request.json.get('email', None)
+    senha = request.json.get('senha',None)
+
+    # Percorre a lista de usuarios para veriicar se as crendencias existem
+    for usuario_id, usuario in usuarios.itens():
+        if usuario[email] == email and usuario[senha] ==senha:
+            access_token = create_access_token(identity=usuario_id)
+            return jsonify(access_token=access_token), 200
+
+
+        return jsonify({"erro: Credenciais invalidas"}), 401
+
 
 # Cria validaçoes de entradas
 class UserSchemas(Schema):
@@ -37,7 +61,7 @@ def criar_usuario():
     #Obter dados vindo da requisiçao e fazer a validaçao
     #(baseando no Schema)
     try:
-        novo_usuario = UsarSchema().load(request.json)
+        novo_usuario = UserSchemas().load(request.json)
     except ValidationError as erro:
         return jsonify(erro.messages), 400
 
