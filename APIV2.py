5492f4e7db92965df3dcbaa849126d31b16d375d
YuriBortolato <yurigbort@gmail.com>
2026-05-05 22:05:11 -0300

    feat: add user input validation schema

---
+++ APIV2.py
@@ -0,0 +1,53 @@
+from importlib.metadata import requires
+
+from flask import Flask, jsonify, request
+from marshmallow import Schema, fields, ValidationError
+
+# Criar aplicação flask
+app = Flask(__name__)
+
+# Criação do banco de dados fictício
+usuarios = {
+    "1":{"nome":"Bob Esponja","email":"cquadrada@bol.com.br","senha":"1234"},
+    "2":{"nome":"Barbie Pedreira","email":"bs2ken@gmail.com","senha":"4321"},
+    "3":{"nome":"Miranha","email":"peterparker@uol.com.br","senha":"mudar123"}
+}
+
+# Cria validaçoes de entradas
+class UserSchemas(Schema):
+    nome = fields.str(requires=True)
+    senha = fields.str(requires=True)
+    email = fields.Email(requires=True)
+
+
+# Rota para pegar usuário por ID
+@app.route("/usuario/<id_usuario>", methods=['GET'])
+def get_usuario(id_usuario):
+    # Buscamos um usuário na lista pelo ID passado na requisição
+    usuario = usuarios.get(id_usuario)
+    if usuario:
+        return jsonify(usuario), 200
+    else:
+        return jsonify({"erro":"Usuário não encontrado!"}), 404
+
+# Rota para cadastrar usuário
+@app.route("/usuario", methods=['POST'])
+def criar_usuario():
+
+    #Obter dados vindo da requisiçao e fazer a validaçao
+    #(baseando no Schema)
+    try:
+        novo_usuario = UsarSchema().load(request.json)
+    except ValidationError as erro:
+        return jsonify(erro.messages), 400
+
+
+    # Criar o ID do novo usuário (tamanho da lista atual +1)
+    id_usuario_novo = str(len(usuarios)+1)
+    # Incluimos o novo usuário na lista
+    usuarios[id_usuario_novo] = novo_usuario
+    return jsonify({"id_usuario":id_usuario_novo}), 201
+
+# Roda a aplicação
+if __name__ == '__main__':
+    app.run()
